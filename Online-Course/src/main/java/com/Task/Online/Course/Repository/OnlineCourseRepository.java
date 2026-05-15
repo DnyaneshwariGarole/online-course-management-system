9b79c063a0d126e3a944d0d45300838efc6ac5bf
Dnyaneshwari_Garole <101723227+DnyaneshwariGarole@users.noreply.github.com>
2026-05-15 22:55:01 +0530

    Remove unnecessary whitespace in OnlineCourseRepository

--- Online-Course/src/main/java/com/Task/Online/Course/Repository/OnlineCourseRepository.java
+++ Online-Course/src/main/java/com/Task/Online/Course/Repository/OnlineCourseRepository.java
@@ -1,10 +1,8 @@
 package com.Task.Online.Course.Repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.Task.Online.Course.Entities.OnlineCourse;
 
 @Repository
